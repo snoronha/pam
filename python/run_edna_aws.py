9b11ad79a90368526460a14f89a603f03512d8c1
Sanjay Noronha <snoronha@gmail.com>
2017-04-27 18:33:44 +0000

    White space

--- python/run_edna_aws.py
+++ python/run_edna_aws.py
@@ -39,16 +39,12 @@ def create_instances(client, n, tag_prefix):
     return instances
 
 def stop_instances(client, instance_ids):
-    response = ec2.stop_instances(
-        InstanceIds=instance_ids
-    )
-    return response
+	response = client.instances.filter(InstanceIds=instance_ids).stop()
+	return response
 
 def terminate_instances(client, instance_ids):
-    response = ec2.terminate_instances(
-        InstanceIds=instance_ids
-    )
-    return response
+	response = client.instances.filter(InstanceIds=instance_ids).terminate()
+	return response
 
 def run_remote_command(host, os_cmd):
     cmd        = "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/ubuntu/.ssh/huff_dev_west.pem ubuntu@" + host + " \"" + os_cmd + "\""
@@ -69,7 +65,7 @@ client        = boto3.resource('ec2')
 instances     = get_running_instances(client)
 print "Instances currently running: ", instances
 tag_prefix    = raw_input('Enter a experiment tag prefix: ')
-new_instances = create_instances(client, 1, tag_prefix)
+new_instances = create_instances(client, 2, tag_prefix)
 up_count      = 0
 while up_count < len(new_instances):
     up_count = 0
@@ -79,28 +75,36 @@ while up_count < len(new_instances):
     print "Number of workers started: %d of %d" % (up_count, len(new_instances))
     if up_count < len(new_instances):
         time.sleep(30)
-print "Sleeping for 1 minute to allow instances to warm up ..."
-time.sleep(60)
+print "Sleeping for 90 seconds to allow instances to warm up ..."
+time.sleep(90)
 print "Experiment started with these instances: ", new_instances
 
 # new_instances = [{'id': 'i-0791b714eee2a4690', 'tag': 'boogie_5_0'}]
 # Divvy the problem up
-MAX_FILE_COUNT = 79999
-file_count     = 0
+MAX_FILE_COUNT  = 79999
+file_count      = 0
+start_num       = 0
+num_per_process = 20
 for instance in new_instances:
-    ip_address = client.Instance(instance['id']).private_ip_address
-    os_cmd     = "cd /home/ubuntu/go/src/anomaly; nohup ../../bin/anomaly 0 20 monthly aws > nohup0.out 2>&1&"
+    end_num     = start_num + num_per_process - 1
+    ip_address  = client.Instance(instance['id']).private_ip_address
+    os_cmd      = "cd /home/ubuntu/go/src/anomaly; nohup ../../bin/anomaly " + str(start_num) + " " + str(end_num) + " monthly aws > nohup0.out 2>&1&"
     out, err, return_code = run_remote_command(ip_address, os_cmd)
     print "ReturnCode: %d, Output: [%s], OutputLen: %d, Err: [%s]" % (return_code, out.rstrip(), len(out.rstrip()), err.rstrip())
+    file_count += 1
+    start_num   = end_num + 1
 
 done = False
 for instance in new_instances:
     ip_address = client.Instance(instance['id']).private_ip_address
     while not(done):
-        print "Process running ..."
+        print "Process running on %s ..." % (ip_address)
         time.sleep(60)
         os_cmd     = "ps auwwx | grep anomaly | grep -v grep"
         out, err, return_code = run_remote_command(ip_address, os_cmd)
         print "ReturnCode: %d, Output: [%s], OutputLen: %d, Err: [%s]" % (return_code, out.rstrip(), len(out.rstrip()), err.rstrip())
         if return_code in [0, 1] and len(out.rstrip()) == 0:
+            print "Termination %s at %s" % (instance['id'], ip_address)
+            terminate_instances(client, [instance['id']])
             done = True
+
